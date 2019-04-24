bab064342aae422e5ad643c26150303d1e2e18a3
nguyentieuat <nguyentieuat@gmail.com>
2019-04-24 11:11:43 -0700

    add file

---
+++ BtUI/BtUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BtUI
+//
+//  Created by Nguyen Thanh Luan on 4/23/19.
+//  Copyright © 2019 Nguyen Thanh Luan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
